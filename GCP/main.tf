2dda95f88f0b2951575835e8c64d15294615cf2b
dil4insolutions <168059706+dil4insolutions@users.noreply.github.com>
2024-04-25 00:43:00 -0500

    Update main.tf

--- GCP/main.tf
+++ GCP/main.tf
@@ -1,5 +1,6 @@
 /**
- * Copyright 2018 Google LLC
+ * dramisetty
+ * Copyright 2024 Innovaslutions 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
